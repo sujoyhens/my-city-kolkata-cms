1eacd4220db86e7b3babd5072d7ad65aa988fb1b
sujoykumarhens <sujoyhens@gmail.com>
2023-10-14 12:59:46 +0530

    autoreload on

--- config/server.js
+++ config/server.js
@@ -6,5 +6,8 @@ module.exports = ({ env }) => ({
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+  }, 
+  autoReload: {
+    enabled: true
+  }
 });
